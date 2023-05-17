f19ed637d346e45caa3aa1ffb271c2599ffac292
RestingWiki <126898951+RestingWiki@users.noreply.github.com>
2023-05-17 19:25:49 +0700

    Removed OpenCV window

--- OOP.py
+++ OOP.py
@@ -83,7 +83,6 @@ class DeviceGUI(tk.Frame):
         self.api_url = tk.StringVar()
         self.credential_key = tk.StringVar()
         self.ESPCAM = tk.StringVar()
-        cv2.namedWindow("Processed Image", cv2.WINDOW_NORMAL)
 
         self.printing_progress = tk.DoubleVar()
         self.printing_progress.set(0)
